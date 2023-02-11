b6c0503b480c036b17f4ee15bf2b175e8dd1c0ea
mo-cherif <mocherifh0@gmail.com>
2023-02-11 15:26:51 +0100

    🍵

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,59 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import './App.css';
+import phone from "./assets/phone.png"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+       <div className='carousels'>
+          <div className='carousel'>
+              <h2>IPHONE 13 PRO</h2>
+              <h3>Oh. So. Pro.</h3>
+              <p>Now in  blue . From $999.00 or 
+                $41.62/mo. for 24/mo.*</p>
+              <img 
+                width={300}
+                src={phone}
+                alt="" 
+              />
+
+
+          </div>
+
+          <div className='carousel'>
+              <h2>IPHONE 13 PRO</h2>
+              <h3>Oh. So. Pro.</h3>
+              <p>Now in  blue . From $999.00 or 
+                $41.62/mo. for 24/mo.*</p>
+              <img 
+                width={300}
+                src={phone}
+                alt="" 
+              />
+
+
+          </div>
+
+          <div className='carousel'>
+              <h2>IPHONE 13 PRO</h2>
+              <h3>Oh. So. Pro.</h3>
+              <p>Now in  blue . From $999.00 or 
+                $41.62/mo. for 24/mo.*</p>
+              <img 
+                width={300}
+                src={phone}
+                alt="" 
+              />
+
+
+          </div>
+
+        
+
+          
+       </div>
     </div>
   )
 }
